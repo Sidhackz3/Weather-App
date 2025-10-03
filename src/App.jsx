970f4d6cc8b71ef04a0315cb7a13d7efc2a0f621
Abhijeet Prajapati <abhijeetkr6790@gmail.com>
2025-10-03 15:49:12 +0530

    Responsive css pushed

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,11 @@ import WeatherDashboard from "./card/WeatherDashboard";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-blue-400 to-indigo-600 flex flex-col items-center">
-      <h1 className="text-3xl font-bold text-white mt-6">Weather App</h1>
+    <>
+      <h1 className="text-3xl font-bold text-black">Weather App</h1>
       <WeatherDashboard />
-    </div>
+
+    </>
   );
 }
 
--- src/card/WeatherDashboard.jsx
+++ src/card/WeatherDashboard.jsx
@@ -1,27 +1,24 @@
 import React, { useState } from "react";
 import WeatherCard from "./WeatherCard";
-import axios from 'axios'
-
+import axios from "axios";
 
 const apiClient = axios.create({
-
-  baseURL: 'https://api.weatherapi.com/v1',
+  baseURL: "https://api.weatherapi.com/v1",
   timeout: 5000,
 });
 
-apiClient.interceptors.response.use(null,async (error) => {
+apiClient.interceptors.response.use(null, async (error) => {
   const config = error.config;
 
-  if (!error.response && config && !config._isRetryRequest){
-    config. _isRetryRequest=true;
-    console.log('Network error, retring request....');
+  if (!error.response && config && !config._isRetryRequest) {
+    config._isRetryRequest = true;
+    console.log("Network error, retrying request....");
     return apiClient(config);
   }
   return Promise.reject(error);
 });
 
-
-const API_KEY = "c9bcc25387b7413ba5491629251909"; 
+const API_KEY = "c9bcc25387b7413ba5491629251909";
 
 const WeatherDashboard = () => {
   const [cityInput, setCityInput] = useState("");
@@ -31,12 +28,12 @@ const WeatherDashboard = () => {
     if (!cityInput) return;
 
     try {
-      const response = await apiClient.get('/current.json',{
+      const response = await apiClient.get("/current.json", {
         params: {
           key: API_KEY,
           q: cityInput,
         },
-        });
+      });
       const data = response.data;
 
       if (data.error) {
@@ -68,31 +65,29 @@ const WeatherDashboard = () => {
   };
 
   return (
-    <div className="mt-10 text-center">
+    <div className=" mt-4 text-center px-4 justify-center">
       {/* Input */}
-      <div className="mb-6">
+      <div className="mb-6 flex flex-col sm:flex-row justify-center items-center gap-2">
         <input
           type="text"
           value={cityInput}
           onChange={(e) => setCityInput(e.target.value)}
           onKeyDown={handleKeyPress}
           placeholder="Enter city name"
-          className="p-2 rounded-l-md border border-gray-300"
+          className="p-2 w-full sm:w-64 rounded-md border border-gray-300"
         />
         <button
           onClick={handleAddCity}
-          className="p-2 bg-blue-500 text-white rounded-r-md hover:bg-blue-600"
+          className="p-2 w-full sm:w-auto bg-blue-500 text-black rounded-md hover:bg-blue-600 "
         >
           Add City
         </button>
       </div>
 
       {/* Cards */}
-      <div className="flex justify-center flex-wrap gap-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {cities.map((data, index) => (
-          <div key={index} className="w-full sm:w-1/2 md:w-1/4">
-            <WeatherCard {...data} />
-          </div>
+          <WeatherCard key={index} {...data} />
         ))}
       </div>
     </div>
